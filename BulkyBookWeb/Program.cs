017c23889c714e57a3e1fa6e517b03c4a31c1ab5
shohag1102 <u1904088@student.cuet.ac.bd>
2026-08-09 17:52:03 +0600

    configure connection string to ef-core

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,3 +1,6 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BulkyBookWeb
 {
     public class Program
@@ -8,6 +11,7 @@ namespace BulkyBookWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
